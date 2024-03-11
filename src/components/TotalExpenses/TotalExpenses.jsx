bb1e4f0aacc3fdece1749f78bf4052fe29581a95
marialapc <marialopez.apc@gmail.com>
2024-03-11 18:22:23 +0100

    Add conditionals to the counter and destructuring for use state

--- src/components/TotalExpenses/TotalExpenses.jsx
+++ src/components/TotalExpenses/TotalExpenses.jsx
@@ -3,26 +3,31 @@ import './TotalExpenses.css'
 import { useState } from 'react'
 
 
-const TotalExpenses = () => {  //destructuring
-    const myState = useState(0)
-    const myStateValue = myState[0];
-    const updateMyState = myState[1];
+const TotalExpenses = () => {  
+    // const myState = useState(0)
+    // const myStateValue = myState[0];
+    // const updateMyState = myState[1];
+    //destructuring
+
+    const [myState, setMystate] = useState(0)
 
     const handleClickAdd = () => {
-        //no mas de 10
-        updateMyState(myStateValue + 1)
+        if (myState < 10) {
+        setMystate(myState+ 1)
+        }
     }
 
     const handleClickRemove = () => {
-        //no menos de 10
-        updateMyState(myStateValue - 1)
+        if (myState > 0) {
+        setMystate(myState - 1)
+        }
     }
 
     return (
         <article className='total-expenses'>
            <h1>Gastado total</h1>
            <section>
-            <h2>{myStateValue}</h2>
+            <h2>{myState}</h2>
            </section>
            <button onClick={handleClickAdd}>+</button>
            <button onClick={handleClickRemove}>-</button>
